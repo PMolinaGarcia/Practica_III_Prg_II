cba7d61af33bb35121af75e565f7fb9911f8d23d
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2025-02-25 19:50:19 +0100

    Subida primeros archivos

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,18 @@
+package aplicacion;
+import mates.*;
+
+/**
+ * Clase para la ejecución del programa. Al ejecutar por terminal, debemos escribir, a continuación de la ejecución y antes de pulsar "enter", el número de puntos con los que queramos trabajar.
+ */
+public class Principal {
+
+    /**
+     * Método para la ejecución del programa.
+     * @param args Es el parámetro numérico que pasamos por terminal al ejecutar el programa.
+     */
+    public static void main(String[] args){
+        System.out.println("El numero PI es " +
+                Matematicas.generarNumeroPiExprLambda(Integer.parseInt(args[0]), x -> 4.0*x/Integer.parseInt(args[0])));
+    }
+}
+
---
+++ src/mates/Matematicas.java
@@ -0,0 +1,55 @@
+package mates;
+
+import java.util.function.*;
+
+
+/**
+ * Clase Matematicas, en la que encontramos los métodos que realizan los cálculos numéricos para obtener una aproximación a pi.
+ */
+public class Matematicas {
+
+    /**
+     * Método para generar puntos aleatorios:
+     * <p>
+     * En primer lugar, se definen las variables coordenadaX y coordenadaY,
+     * que son las que tendrá cada punto generado y cuyos valores serán aleatorios;
+     * para esto, empleamos el método random(), de la librería Math de Java.
+     * <p>
+     * Después, si el punto generado por dichas coordenadas cumple que está dentro o en el borde de un círculo de radio 1,
+     * aumentaremos en 1 el contador de dardos, indicando que ahora hay un dardo más dentro;
+     * si no se da el caso de que el punto está dentro o en el borde del círculo, no sucede nada.
+     */
+    public static int generarPuntoAleatorio(){
+        double coordenadaX = Math.random();
+        double coordenadaY = Math.random();
+
+        if (coordenadaX*coordenadaX+coordenadaY*coordenadaY<=1){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * Método funcional para calcular el número pi a partir de los puntos generados:
+     * <p>
+     *
+     * @param pasos Es el parámetro numérico que indica la cantidad de puntos totales con los que queremos trabajar.
+     * @param funcion Es el parámetro de tipo DoubleUnaryOperator que indica la funcion con la que se van a realizar los cálculos.
+     * @return Retornamos la aplicación de la función, que, según la que hayamos pasado como parámetros,
+     * realizará el cálculo sustituyendo la variable que hayamos escrito por dardosDentro.
+     */
+
+    public static double generarNumeroPiExprLambda(long pasos, DoubleUnaryOperator funcion){
+
+        long dardosDentro = 0;
+
+        for (int j = 0; j<pasos; j++){
+            dardosDentro+=generarPuntoAleatorio();
+        }
+            return funcion.applyAsDouble(dardosDentro);
+
+    }
+}
+
